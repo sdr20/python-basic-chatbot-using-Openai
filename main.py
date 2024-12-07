ecf1ca5808789d861665d719601520a5e58e5dc0
Steven DR <145115889+sdr20@users.noreply.github.com>
2024-12-07 21:51:19 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import openai
+
+openai.api_key = "yourgptopenaiAPI"
+
+def chat_with_gpt(prompt):
+    response = openai.ChatCompletion.create(
+        model="your gpt version  ex:gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    
+    return response.choices[0].message.content.strip()
+
+if __name__ == "__main__":
+    while True:
+        user_input = input("you: ")
+        if user_input.lower() in ["quit", "exit", "bye"]:
+            break
+        
+        
+        response = chat_with_gpt(user_input)
+        print("Chatbot: ", response)
